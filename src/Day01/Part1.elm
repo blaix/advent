e0ead8cf73ce481451dd485b6cdc437bfbb4772e
Justin Blake <justin@blaix.com>
2021-12-02 23:25:18 -0500

    day 1 part 1
    Focus on readable, self-documenting code.

---
+++ src/Day01/Part1.elm
@@ -0,0 +1,87 @@
+module Day01.Part1 exposing (..)
+
+import Html exposing (Html, text)
+
+
+main : Html msg
+main =
+    text (Debug.toString answer)
+
+
+answer : Int
+answer =
+    input
+        |> getMeasurements
+        |> pairWithNextMeasurement
+        |> getDepthChanges
+        |> countIncreases
+
+
+type DepthChange
+    = Increase
+    | Decrease
+    | NoChange
+
+
+type alias Measurement =
+    Int
+
+
+getMeasurements : String -> List Measurement
+getMeasurements rawInput =
+    rawInput
+        |> String.trim
+        |> String.lines
+        |> List.filterMap String.toInt
+
+
+pairWithNextMeasurement : List Measurement -> List ( Measurement, Measurement )
+pairWithNextMeasurement measurements =
+    let
+        offsetMeasurements =
+            List.drop 1 measurements
+    in
+    List.map2 Tuple.pair measurements offsetMeasurements
+
+
+getDepthChanges : List ( Measurement, Measurement ) -> List DepthChange
+getDepthChanges pairedMeasurements =
+    List.map toDepthChange pairedMeasurements
+
+
+toDepthChange : ( Measurement, Measurement ) -> DepthChange
+toDepthChange ( m1, m2 ) =
+    if m1 < m2 then
+        Increase
+
+    else if m1 > m2 then
+        Decrease
+
+    else
+        NoChange
+
+
+countIncreases : List DepthChange -> Int
+countIncreases depthChanges =
+    List.filter (\x -> x == Increase) depthChanges
+        |> List.length
+
+
+
+-- Sample input for now.
+
+
+input : String
+input =
+    """
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"""
