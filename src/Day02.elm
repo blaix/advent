2f7bb1ce496364dbd41bbac4a3224c870f643d3f
Justin Blake <justin@blaix.com>
2021-12-05 08:21:31 -0500

    Started chipping away at day 2...
    ...but realized I need real navigation first.

--- src/Day02.elm
+++ src/Day02.elm
@@ -10,12 +10,22 @@ forward 8
 up 3
 down 8
 forward 2
-"""
+""" |> String.trim
 
 
+
+-- TODO: Unfortunately it is time to handle navigation...
+-- Otherwise elm-live will reset things to Day01.part1 whenever I make a change.
+-- See: https://guide.elm-lang.org/webapps/navigation.html
+
+
+part1 : String -> String
 part1 rawInput =
     rawInput
+        |> String.lines
+        |> Debug.toString
 
 
+part2 : String -> String
 part2 rawInput =
     rawInput
